c14e8a549baaa71189b941852a55e87ecbbb11f8
Yinan Zhou <yinan.zhou@atalent.com>
2026-09-20 14:47:51 +0800

    Prepare 360 release: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 
 export default defineConfig({
+  build: { assets: 'astro-360-assets' },
   integrations: [
     react(),
     ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()]),
